7b1d4d0ba1d59f0d61a5a0684802b4f7f883c412
Ian Geckeler <ikcgeckeler@gmail.com>
2019-08-19 14:10:08 -0400

    got webpack working
    Co-authored-by: Camera Walrond <@camerawalrond@gmail.com>
    Co-authored-by: Ian Geckeler <@ikcgeckeler@gmail.com>

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  //...
+  entry: "./client/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "client/src/index.html"
+    })
+  ]
+};
