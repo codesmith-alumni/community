c3c9e120524b5b03012583f39002fc81a8c6fbe9
Giuseppe Zep Valentino <zepvalue@gmail.com>
2019-08-19 16:49:06 -0400

    (UI) SignUp SignIn

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,16 +1,18 @@
 // import { hot } from "react-hot-loader/root";
-import React from "react";
-import ReactDOM from "react-dom";
-import FeedPage from "../containers/FeedPage.jsx";
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        Hello world!
-        <FeedPage />
-      </div>
-    );
-  }
-}
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import FeedPage from '../containers/FeedPage';
+import SignIn from './SignIn';
+import SignUp from './SignUp';
+
+const App = () => {
+  return (
+    <Router>
+      <Route path="/" exact component={SignIn} />
+      <Route path="/signUp/" component={SignUp} />
+      <Route path="/feed/" component={FeedPage} />
+    </Router>
+  );
+};
 
 export default App;
