d408b6b9ad54ec33ff8c89971444e629b4966541
Ian Geckeler <ikcgeckeler@gmail.com>
2019-08-19 15:23:45 -0400

    added styled components

--- client/containers/FeedPage.jsx
+++ client/containers/FeedPage.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import { connect } from "react-redux";
+import styled from "styled-components";
 import { toggle } from "../actions/toggle";
+
+const FeedPageStyles = styled.div`
+  background-color: blue;
+  height: 150px;
+  width: 100%;
+`;
 
 const mapStateToProps = state => ({
   posts: state.posts
@@ -13,9 +20,11 @@ class FeedPage extends React.Component {
   render() {
     return (
       <div>
-        I am the feed page.
-        <h1>value of toggled: {this.props.posts[0].isOpen.toString()}</h1>
-        <button onClick={index => this.props.togglePost(0)}> toggle</button>
+        <FeedPageStyles>
+          Feed I am the feed page.
+          <h1>value of toggled: {this.props.posts[0].isOpen.toString()}</h1>
+          <button onClick={index => this.props.togglePost(0)}> toggle</button>
+        </FeedPageStyles>
       </div>
     );
   }
