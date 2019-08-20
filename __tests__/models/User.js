abd08a1b2effd848a038656fa04e517d50777c96
Ian Geckeler <ikcgeckeler@gmail.com>
2019-08-20 11:58:49 -0400

    user validation works and has tests

--- __tests__/models/User.js
+++ __tests__/models/User.js
@@ -1,10 +1,15 @@
 const { reset, end } = require("../../server/db-reset");
 const User = require("../../server/models/User");
+const pool = require("../../server/database");
 
 describe("Tests for User Model.", () => {
   beforeAll(done => {
     reset().then(() => done());
   });
+  afterEach(done => {
+    reset().then(() => done());
+  });
+
   it("User saves into the database.", done => {
     const user = new User("Ian", "test@test.com", "password");
     try {
@@ -20,6 +25,48 @@ describe("Tests for User Model.", () => {
     } catch (err) {
       throw new Error("Save operation failed:", err);
     }
+  });
+
+  describe("Tests for user", () => {
+    beforeAll(done => {
+      pool
+        .query(
+          "INSERT INTO users(name, email, password, karma) VALUES($1, $2, $3,$4)",
+          ["test", "test@test.com", "testpass", 0]
+        )
+        .then(() => {
+          done();
+        })
+        .catch(() => {
+          done();
+        });
+    });
+
+    it("Validates User with right password.", done => {
+      const user = new User("test", "test@test.com", "testpass");
+      user
+        .validateUser()
+        .then(res => {
+          expect(res.rows.length).toBeGreaterThan(0);
+          done();
+        })
+        .catch(err => {
+          throw err;
+        });
+    });
+
+    it("Properly validates User with wrong password.", done => {
+      const user = new User("testy", "test@test.com", "testpass");
+      user
+        .validateUser()
+        .then(res => {
+          expect(res.rows.length).toBe(0);
+          done();
+        })
+        .catch(err => {
+          throw err;
+        });
+    });
   });
   afterAll(() => end());
 });
--- server/models/User.js
+++ server/models/User.js
@@ -22,6 +22,11 @@ class User {
     // query the database to see if there exists a user in database... if so validate the password...
     // if password matches throw user or password does not match
     //if not throw an error
+    const sql = `
+    SELECT * FROM users WHERE (email = $1) AND (password = $2)
+    `;
+    const values = [this.email, this.password];
+    return pool.query(sql, values);
   }
 
   /**
