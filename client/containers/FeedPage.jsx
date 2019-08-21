f4f8bf02393e314362ee051f02b72a53237d4de1
Giuseppe Zep Valentino <zepvalue@gmail.com>
2019-08-20 22:49:55 -0400

    (UI) Nav and Signout

--- client/containers/FeedPage.jsx
+++ client/containers/FeedPage.jsx
@@ -1,70 +1,62 @@
-import React from "react";
-import { connect } from "react-redux";
-import styled from "styled-components";
-import { toggle, updatePosts } from "../actions/actions";
-import   Search   from "../containers/Search.jsx"
-import Feed from "../containers/Feed.jsx"
-import Composer from "../containers/Composer.jsx"
+import React from 'react';
+import { connect } from 'react-redux';
+import styled from 'styled-components';
+import { toggle, updatePosts } from '../actions/actions';
+import Search from '../containers/Search.jsx';
+import Feed from '../containers/Feed.jsx';
+import Composer from "./Composer.jsx";
 
 const FeedPageStyles = styled.div`
   background-color: lightcoral;
   height: 150px;
   width: 100%;
   border: solid;
-
 `;
 
 const mapStateToProps = store => ({
-  posts: store.posts
+  posts: store.posts,
 });
 
 const mapDispatchToProps = dispatch => ({
   togglePost: index => dispatch(toggle(index)),
-  updatePosts: posts => dispatch(updatePosts(posts))
+  updatePosts: posts => dispatch(updatePosts(posts)),
 });
 
 class FeedPage extends React.Component {
-  constructor(props){
-    super(props)
-this.state = {}
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-  } 
-
-  componentDidMount(){
-
+  componentDidMount() {
     fetch('/posts/')
       .then(response => response.json())
       .then(response => {
-        const posts = []
-      response.forEach(item => {
-        posts.push({
-          isOpen: false,
-          company: item.company,
-          details:item.content,
-          username:item.user_id
+        const posts = [];
+        response.forEach(item => {
+          posts.push({
+            isOpen: false,
+            company: item.company,
+            details: item.content,
+            username: item.user_id,
+          });
+        });
 
-        })
-     
+        return this.props.updatePosts(posts);
       })
-        
-       return this.props.updatePosts(posts)})
-      .catch(err => console.log(err))
-
+      .catch(err => console.log(err));
   }
- 
 
   render() {
-
     return (
-        <FeedPageStyles>
-          Feed I am the feed page.
-        <Search/>
-        <Composer/>
-        <Feed posts = {this.props.posts}/>
-          <h1>value of toggled: {this.props.posts[0].isOpen.toString()}</h1>
-          <button onClick={index => this.props.togglePost(0)}> toggle</button>
-        </FeedPageStyles>
-
+      <FeedPageStyles>
+        Feed I am the feed page.
+        <Search />
+        <Composer />
+        <Feed posts={this.props.posts} />
+        <h1>value of toggled: {this.props.posts[0].isOpen.toString()}</h1>
+        <button onClick={index => this.props.togglePost(0)}> toggle</button>
+      </FeedPageStyles>
     );
   }
 }
